6c32c87a2ab1c6a7eb98ebcdeb95cc3b75bb3093
kaut0 <zilullah792@gmail.com>
2020-08-22 13:55:28 +0800

    siap siap pulang

--- include/headerAdmin.php
+++ include/headerAdmin.php
@@ -53,17 +53,6 @@
                 </li>
                 <li>
                     <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
-                    <ul class="collapse list-unstyled" id="pageSubmenu">
-                        <li>
-                            <a href="#">Page 1</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 2</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 3</a>
-                        </li>
-                    </ul>
                 </li>
                 <li>
                     <a href="./AdminFaq.php">Faq</a>
